321fcf848c5a9d2131155a339206aafab57f43de
User <nevo.krien@gmail.com>
2023-10-16 19:28:57 +0300

    events adding works

--- lib/ai_assistant_web/live/calendar_live.ex
+++ lib/ai_assistant_web/live/calendar_live.ex
@@ -9,12 +9,14 @@ defmodule AiAssistantWeb.NeedDateTime do
 
       # Common functionality for handling events
       def handle_event("receive_time", %{"current_time" => time}, socket) do
+        #IO.puts('recived')
         parsed_time = 
           case Timex.parse(time, "{ISO:Extended}") do
-            {:ok, datetime} -> Timex.format!(datetime, "{RFC1123}")
+            {:ok, datetime} -> datetime#Timex.format!(datetime, "{RFC1123}")
             {:error, _} -> "Invalid time format received"
           end
 
+        #IO.inspect(parsed_time, label: 'client retrieved time')#when this goes away things work fine???? data race with the mount???
         {:noreply, assign(socket, current_time: parsed_time)}
       end
 
@@ -35,8 +37,7 @@ defmodule AiAssistantWeb.CalendarLive do
       socket
       |> assign(:event_form, %{})
       |> assign(:current_user_id, session["current_user_id"])
-      |> assign(:current_time, "Not received yet")
-      #|> assign(:myself, socket.id) 
+      |> assign_new(:current_time, fn -> "Not received yet" end)
 
     {:ok, socket}
   end
@@ -57,6 +58,8 @@ defmodule AiAssistantWeb.CalendarLive do
           
           <label for="event_date">Event Date:</label>
           <input type="date" name="event[date]" id="event_date"/>
+          <input type="time" name="event[time]" id="event_time" step="1">
+
 
           <input type="submit" value="Add Event" />
         </form>
@@ -67,39 +70,48 @@ defmodule AiAssistantWeb.CalendarLive do
 
   @impl true
   def handle_event("save_event", %{"event" => event_params}, socket) do
+    IO.puts('save event triggered')
     current_user_id = socket.assigns.current_user_id
 
-    # You might need to parse the date if it's submitted as a string, 
-    # or ensure it's in the correct format for the changeset function.
-    attrs = Map.put(event_params, "user_id", current_user_id)
+    # Extracting date and time from the event_params
+    date = Map.fetch!(event_params, "date")
+    time = Map.get(event_params, "time") || "00:00" # default to midnight if time is not provided
+
+    # Constructing a string that represents the date and time
+    datetime_string = date <> "T" <> time #<> ":00" # appending seconds, if your time input doesn't include them
+
+    # Parsing the string into a NaiveDateTime struct.
+    naive_datetime = NaiveDateTime.from_iso8601!(datetime_string)
+    
+    # Convert the naive datetime to UTC datetime
+    utc_datetime = DateTime.from_naive!(naive_datetime, "Etc/UTC")
+
+    # Add the datetime and user_id to the event_params
+    attrs =  %{"date" => utc_datetime, "user_id" => current_user_id, "description"=>Map.fetch!(event_params, "description")}
+
+    IO.inspect(attrs, label: 'event')
 
     case Event.create_event(attrs) do
       {:ok, _event} ->
         # If the event is saved successfully, you might want to clear the form or emit a success message.
-        {:noreply, socket |> put_flash(:info, "Event created successfully.")}
-
-      {:error, _changeset} ->
+        {:noreply, socket}
+      {:error, changeset} ->
         # If there's an error, you'll need to handle it here, possibly sending back validation errors to the form.
-        {:noreply, socket |> put_flash(:error, "Failed to create event.")}
+        IO.inspect(changeset)
+        {:noreply, socket}
     end
   end
 
 
 
-  def formatted_time(time) do
-    case time do
-      %DateTime{} ->
-        time
-        |> DateTime.to_naive() # Convert to NaiveDateTime, dropping timezone info
-        |> NaiveDateTime.to_string() # Convert to string
+  def formatted_time(datetime) when is_binary(datetime), do: datetime
 
-      %NaiveDateTime{} ->
-        NaiveDateTime.to_string(time) # Already a NaiveDateTime, just convert to string
-
-      _ ->
-        "Invalid time format" # or handle this case as per your requirement
-      end
+  def formatted_time(%DateTime{} = datetime) do
+    "#{datetime.year}-#{datetime.month}-#{datetime.day} #{datetime.hour}:#{datetime.minute}"
   end
+
+  def formatted_time(_), do: "Unexpected input"
+
 
   #defp formatted_time(time) when is_binary(time), do: time
   #defp formatted_time("Not received yet"), do: "Not received yet"
